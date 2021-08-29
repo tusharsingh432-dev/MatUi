5be0ec2686f9f835e3360528e3075b050be7bed8
Tushar Singh <tusharsingh432@gmail.com>
2021-08-29 23:23:59 +0530

    Sidebar And ToolBar

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import SideMenu from '../components/SideMenu';
+import Header from '../components/Header';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    appMain:{
+      paddingLeft: '320px',
+      width: '100%'
+    }
+})
+
 
 function App() {
+  const classes = useStyles();
   return (
-      <div>
+    <>
+      <SideMenu />
+      <Header />
+      <div className={classes.appMain}>
         Hello Matui
       </div>
+    </>
   );
 }
 
