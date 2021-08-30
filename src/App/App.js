8f6044fc036534ab5a8c427132530ef43e7cae26
Tushar Singh <tusharsingh432@gmail.com>
2021-08-30 22:49:37 +0530

    Page Header Complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,57 @@
-import React from 'react';
-import './App.css';
-import SideMenu from '../components/SideMenu';
-import Header from '../components/Header';
-import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
+import React from "react";
+import "./App.css";
+import SideMenu from "../components/SideMenu";
+import Header from "../components/Header";
+import {
+  createTheme,
+  CssBaseline,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import PageHeader from "../components/PageHeader";
+import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
 
-const theme = createMuiTheme({
-  palette:{
-    primary:{
-      main:"#333996",
-      light:"#3c44b126"
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44b126",
     },
-    secondary:{
-      main:"#f83245",
-      light:"#f8324526"
-    }
-  }
-})
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526",
+    },
+  },
+  zIndex: {
+    appBar: 90,
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true,
+    },
+  },
+});
 
 const useStyles = makeStyles({
-    appMain:{
-      paddingLeft: '320px',
-      width: '100%'
-    }
-})
-
+  appMain: {
+    paddingLeft: "320px",
+    width: "100%",
+  },
+});
 
 function App() {
   const classes = useStyles();
   return (
-    <ThemeProvider theme = {theme}>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+
+        <PageHeader
+          icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
+          title="Title"
+          subtitle="Sub Title"
+        />
       </div>
       <CssBaseline />
     </ThemeProvider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,36 +9,42 @@ import {
 } from "@material-ui/core";
 import React from "react";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
-import { ChatBubbleOutline, PowerSettingsNewOutlined, SearchOutlined } from "@material-ui/icons";
+import {
+  ChatBubbleOutline,
+  PowerSettingsNewOutlined,
+  SearchOutlined,
+} from "@material-ui/icons";
 
 const useStyle = makeStyles({
-    root:{
-        backgroundColor:'#fff'
+  root: {
+    backgroundColor: "#fff",
+    transform: "translateƵ(0)",
+    position: "relative",
+  },
+  searchInput: {
+    opacity: "0.6",
+    padding: "0px 8px",
+    fontSize: "0.8rem",
+    "&:hover": {
+      backgroundColor: "#f2f2f2",
     },
-    searchInput:{
-        opacity : '0.6',
-        padding : '0px 8px',
-        fontSize : '0.8rem',
-        '&:hover' :{
-            backgroundColor: '#f2f2f2'
-        },
-        '& .MuiSvgIcon-root':{
-            marginRight : '8px'
-        }
-    }
-})
+    "& .MuiSvgIcon-root": {
+      marginRight: "8px",
+    },
+  },
+});
 
 export default function Header() {
-    const classes = useStyle();
+  const classes = useStyle();
   return (
     <AppBar position="static" className={classes.root}>
       <Toolbar>
         <Grid container alignItems="center">
           <Grid item>
-            <InputBase 
-                placeholder="Search"
-                className={classes.searchInput}
-                startAdornment = {<SearchOutlined fontSize = "small" />}
+            <InputBase
+              placeholder="Search"
+              className={classes.searchInput}
+              startAdornment={<SearchOutlined fontSize="small" />}
             />
           </Grid>
           <Grid item sm></Grid>
